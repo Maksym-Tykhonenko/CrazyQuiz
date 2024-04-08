17aec0dacf39a55cc5c7f2709ef0dfbddf47b125
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-04-08 18:14:47 +0300

    save

--- crazyQuiz/App.js
+++ crazyQuiz/App.js
@@ -1,11 +1,86 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('ProfileScreen');
+        }}>
+        <Text>Go to Profile</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('LelSelectionScreen');
+        }}>
+        <Text>Go to Lvls Screen</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('RulseScreen');
+        }}>
+        <Text>Go to Rules</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+function ProfileScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>ProfileScreen Screen</Text>
+    </View>
+  );
+}
+function LelSelectionScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>LelSelectionScreen Screen</Text>
+    </View>
+  );
+}
+
+function RulseScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>RulseScreen Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Hello Crazy Quiz!!!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="ProfileScreen"
+          component={ProfileScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="LelSelectionScreen"
+          component={LelSelectionScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="RulseScreen"
+          component={RulseScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
