d0b5bf29cb54732fa0932f02651b36ab1a2b9b01
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-04-24 18:46:50 +0300

    1.0.3(8)

--- crazyQuiz/App.js
+++ crazyQuiz/App.js
@@ -2,6 +2,11 @@ import React, {useEffect, useRef, useState} from 'react';
 import {Text, TouchableOpacity, View, Animated} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ReactNativeIdfaAaid, {
+  AdvertisingInfoResponse,
+} from '@sparkfabrik/react-native-idfa-aaid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
 
 import HomeScreen from './screens/HomeScreen';
 import RulseScreen from './screens/RulseScreen';
@@ -21,69 +26,119 @@ import Lvl8 from './screens/Lvl8';
 import Lvl9 from './screens/Lvl9';
 import Lvl10 from './screens/Lvl10';
 import WinnerScreen from './screens/WinnerScreen';
+import Prod from './screens/Prod';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [louderIsEnded, setLouderIsEnded] = useState(false);
-  //////////// LOADER
-  const appearingAnim = useRef(new Animated.Value(0)).current;
-  const appearingSecondAnim = useRef(new Animated.Value(0)).current;
+  ///////////// Отримання IDFA
+  const [idfa, setIdfa] = useState(null);
+  console.log('idfa==>', idfa);
 
   useEffect(() => {
-    Animated.timing(appearingAnim, {
-      toValue: 1,
-      duration: 2000,
-      useNativeDriver: true,
-    }).start();
+    getData();
   }, []);
 
   useEffect(() => {
-    setTimeout(() => {
-      Animated.timing(appearingSecondAnim, {
-        toValue: 1,
-        duration: 2500,
-        useNativeDriver: true,
-      }).start();
-      //setLouderIsEnded(true);
-    }, 1000);
-  }, []);
+    setData();
+  }, [idfa]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLouderIsEnded(true);
-    }, 4000);
-  }, []);
+  const setData = async () => {
+    try {
+      const data = {
+        idfa,
+      };
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem('App', jsonData);
+      console.log('Дані збережено в AsyncStorage');
+    } catch (e) {
+      console.log('Помилка збереження даних:', e);
+    }
+  };
 
-  return (
-    <NavigationContainer>
-      {!louderIsEnded ? (
-        <View
-          style={{
-            position: 'relative',
-            flex: 1,
-            //backgroundColor: 'rgba(225, 7, 6, 255)',
-          }}>
-          <Animated.Image
-            source={require('./assets/rediz/loader1.jpg')} // Special animatable View
-            style={{
-              //...props.style,
-              opacity: appearingAnim,
-              height: '100%',
-              position: 'absolute', // Bind opacity to animated value
-            }}
+  const getData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem('App');
+      if (jsonData !== null) {
+        const parsedData = JSON.parse(jsonData);
+        console.log('Дані дістаються в AsyncStorage');
+        console.log('parsedData in App==>', parsedData);
+        setIdfa(parsedData.idfa);
+      } else {
+        await fetchIdfa();
+        await someFunction();
+      }
+    } catch (e) {
+      console.log('Помилка отримання даних:', e);
+    }
+  };
+
+  const fetchIdfa = async () => {
+    try {
+      const res = await ReactNativeIdfaAaid.getAdvertisingInfo();
+      if (!res.isAdTrackingLimited) {
+        setIdfa(res.id);
+      } else {
+        setIdfa(true);
+      }
+    } catch (err) {
+      console.log('err', err);
+      setIdfa(null);
+      fetchIdfa(); //???
+    }
+  };
+
+  //  OneSignall
+  //  d96c95fa-9f3c-454f-80d4-2035da621d75
+  const requestPermission = () => {
+    return new Promise((resolve, reject) => {
+      try {
+        OneSignal.Notifications.requestPermission(true);
+        resolve(); // Викликаємо resolve(), оскільки OneSignal.Notifications.requestPermission не повертає проміс
+      } catch (error) {
+        reject(error); // Викликаємо reject() у разі помилки
+      }
+    });
+  };
+
+  // Виклик асинхронної функції requestPermission() з використанням async/await
+  const someFunction = async () => {
+    try {
+      await requestPermission();
+      // Якщо все Ok
+    } catch (error) {
+      console.log('err в someFunction==> ', error);
+    }
+  };
+
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  // OneSignal Initialization
+  OneSignal.initialize('d96c95fa-9f3c-454f-80d4-2035da621d75');
+
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
+  //Add Data Tags
+  OneSignal.User.addTag('key', 'value');
+
+  //Route
+  const [route, setRoute] = useState();
+  const Route = ({isFatch}) => {
+    if (isFatch) {
+      return (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Prod"
+            component={Prod}
+            options={{headerShown: false}}
+            initialParams={{idfa: idfa}}
           />
-          <Animated.Image
-            source={require('./assets/rediz/loader2.jpg')} // Special animatable View
-            style={{
-              //...props.style,
-              opacity: appearingSecondAnim,
-              height: '100%',
-              position: 'absolute', // Bind opacity to animated value
-            }}
-          />
-        </View>
-      ) : (
+        </Stack.Navigator>
+      );
+    } else {
+      return (
         <Stack.Navigator>
           <Stack.Screen
             name="FirstCongratScreen"
@@ -176,6 +231,95 @@ const App = () => {
             options={{headerShown: false}}
           />
         </Stack.Navigator>
+      );
+    }
+  };
+
+  //////////// useEffect що виріш який шлях включати
+  useEffect(() => {
+    const url = `https://terrific-eminent-win.space/FG8hKVbd`;
+    const targetD = new Date('2024-04-29T12:00:00'); //дата з якої поч працювати prod
+    const currentD = new Date(); //текущая дата
+
+    if (currentD <= targetD) {
+      setRoute(false);
+    } else {
+      fetch(url)
+        .then(r => {
+          if (r.status === 200) {
+            setRoute(true);
+          } else {
+            setRoute(false);
+          }
+        })
+        .catch(e => {
+          console.log('errar', e);
+          setRoute(false);
+        });
+    }
+  }, []);
+
+  //////////// LOADER
+  const [louderIsEnded, setLouderIsEnded] = useState(false);
+
+  const appearingAnim = useRef(new Animated.Value(0)).current;
+  const appearingSecondAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(appearingAnim, {
+      toValue: 1,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      Animated.timing(appearingSecondAnim, {
+        toValue: 1,
+        duration: 3000,
+        useNativeDriver: true,
+      }).start();
+      //setLouderIsEnded(true);
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLouderIsEnded(true);
+    }, 7000);
+  }, []);
+
+  return (
+    <NavigationContainer>
+      {!louderIsEnded ? (
+        <View
+          style={{
+            position: 'relative',
+            flex: 1,
+            //backgroundColor: 'rgba(225, 7, 6, 255)',
+          }}>
+          <Animated.Image
+            source={require('./assets/rediz/loader1.jpg')} // Special animatable View
+            style={{
+              //...props.style,
+              opacity: appearingAnim,
+              height: '100%',
+              position: 'absolute', // Bind opacity to animated value
+            }}
+          />
+          <Animated.Image
+            source={require('./assets/rediz/loader2.jpg')} // Special animatable View
+            style={{
+              //...props.style,
+              opacity: appearingSecondAnim,
+              height: '100%',
+              position: 'absolute', // Bind opacity to animated value
+            }}
+          />
+        </View>
+      ) : (
+        <Route isFatch={route} />
       )}
     </NavigationContainer>
   );
